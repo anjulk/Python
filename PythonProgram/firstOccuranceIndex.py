cbec4fb37030b82536cab477d5dfd764099419f6
anjulk <anjulkumar2001@gmail.com>
2023-11-24 11:56:42 -0500

    Find first occurance of a string inside another string and return Index

---
+++ PythonProgram/firstOccuranceIndex.py
@@ -0,0 +1,32 @@
+def firstOccuranceIndex( ):
+    inputString = input('Input your string  ')
+    firstOccurance = input('Input string to be found ')
+    print(inputString , firstOccurance)
+    l1 = len(inputString)
+    l2 = len(firstOccurance)
+    
+    if l2 == 0:
+        return -1
+    elif l2 > l1:
+        return -1
+    else:
+        counter = 0
+        position = -1
+        for i in range(l1):
+            if firstOccurance[counter] == inputString[i]:
+                counter += 1
+            else:
+                counter = 0
+            if counter == 1:
+                position = i
+            
+            if counter == l2:
+                return position
+        return -1
+
+
+def main():
+    print('Index of String is at %d' % (firstOccuranceIndex()))
+
+if __name__ == '__main__':
+    main()
